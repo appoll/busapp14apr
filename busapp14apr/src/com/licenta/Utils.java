ca85f02112ab6544be8d74de30462235c077f2b7
Paul Anton <paul.antton@gmail.com>
2014-04-24 18:56:48 +0300

    Final Predictions algorithm

--- busapp14apr/src/com/licenta/Utils.java
+++ busapp14apr/src/com/licenta/Utils.java
@@ -3,8 +3,19 @@ package com.licenta;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 
 public class Utils {
+	
+	static final long ONE_MINUTE_IN_MILLIS=60000;
+	static final long ONE_SECOND_IN_MILLIS=1000;
+	
 	public static long secondsToArrival (String time)
 	{
 		Calendar current = Calendar.getInstance();
@@ -41,5 +52,133 @@ public class Utils {
 
 		long seconds = (d2.getTime()-d1.getTime())/1000;
 		return seconds;	
+	}
+	
+	
+	public static String getServiceID ()				// în funcție de zi
+	{
+		String id = "";
+		Calendar current = Calendar.getInstance();
+		Locale locale = Locale.getDefault();
+		
+		String day = current.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, locale);
+		day = day.toLowerCase();
+		
+		return id;
+	}
+	
+	public static int getHeadway (String trip_id)
+	{
+		Calendar current = Calendar.getInstance();
+		Calendar start = new GregorianCalendar(current.get(Calendar.YEAR),
+												current.get(Calendar.MONTH),
+												current.get(Calendar.DAY_OF_MONTH));
+		
+		Calendar end = new GregorianCalendar(current.get(Calendar.YEAR),
+											current.get(Calendar.MONTH),
+											current.get(Calendar.DAY_OF_MONTH));
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query frequencyQ = new Query ("Frecvente");
+		PreparedQuery frequencyPQ = datastore.prepare(frequencyQ);
+		
+		for (Entity freq : frequencyPQ.asIterable())
+		{
+			if (freq.getProperty("trip_id").toString().equals(trip_id))
+			{
+				String start_time = freq.getProperty("start_time").toString();
+				String end_time = freq.getProperty("end_time").toString();
+				String [] tokens_start = start_time.split(":");
+				String [] tokens_end = end_time.split(":");
+				
+				start.set(Calendar.HOUR_OF_DAY, Integer.parseInt(tokens_start[0]));
+				start.set(Calendar.MINUTE, Integer.parseInt(tokens_start[1]));
+				start.set(Calendar.SECOND, Integer.parseInt(tokens_start[2]));
+				
+				end.set(Calendar.HOUR_OF_DAY, Integer.parseInt(tokens_end[0]));
+				end.set(Calendar.MINUTE, Integer.parseInt(tokens_end[1]));
+				end.set(Calendar.SECOND, Integer.parseInt(tokens_end[2]));
+				
+				Date currentDate = current.getTime();
+				Date startDate = start.getTime();
+				Date endDate = end.getTime();
+				
+	//			System.out.println("de ce nu ? " + freq.getProperty("trip_id"));
+				
+				
+	//			System.out.println("START : " + startDate);
+	//			System.out.println("CURENTA : " + currentDate);
+	//			System.out.println("FINAL : " + endDate);
+				
+					if (currentDate.after(startDate) && currentDate.before(endDate))
+					{	
+					//	System.out.println("DA DA DA DA DA DA DA DA DA DA DA DA DA DA");
+					//	System.out.println(freq.getProperty("trip_id"));
+					//	System.out.println("intervalul"+ freq.getProperty("start_time")+" și " + freq.getProperty("end_time"));
+						
+						return Integer.parseInt(freq.getProperty("headway_secs").toString());
+					}
+			}
+		}
+		return 0;
+	}
+	
+	
+	public static long getNextArrival (String trip_id)
+	{
+		Calendar current = Calendar.getInstance();
+		Calendar start = new GregorianCalendar(current.get(Calendar.YEAR),
+												current.get(Calendar.MONTH),
+												current.get(Calendar.DAY_OF_MONTH));
+		
+		Calendar end = new GregorianCalendar(current.get(Calendar.YEAR),
+											current.get(Calendar.MONTH),
+											current.get(Calendar.DAY_OF_MONTH));
+		
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query frequencyQ = new Query ("Frecvente");
+		PreparedQuery frequencyPQ = datastore.prepare(frequencyQ);
+		
+		for (Entity freq : frequencyPQ.asIterable())
+		{
+			if (freq.getProperty("trip_id").toString().equals(trip_id))
+			{
+				String start_time = freq.getProperty("start_time").toString();
+				String end_time = freq.getProperty("end_time").toString();
+				String [] tokens_start = start_time.split(":");
+				String [] tokens_end = end_time.split(":");
+				
+				start.set(Calendar.HOUR_OF_DAY, Integer.parseInt(tokens_start[0]));
+				start.set(Calendar.MINUTE, Integer.parseInt(tokens_start[1]));
+				start.set(Calendar.SECOND, Integer.parseInt(tokens_start[2]));
+				
+				end.set(Calendar.HOUR_OF_DAY, Integer.parseInt(tokens_end[0]));
+				end.set(Calendar.MINUTE, Integer.parseInt(tokens_end[1]));
+				end.set(Calendar.SECOND, Integer.parseInt(tokens_end[2]));
+				
+				Date currentDate = current.getTime();
+				Date startDate = start.getTime();
+				Date endDate = end.getTime();
+				
+					if (currentDate.after(startDate) && currentDate.before(endDate))
+					{	
+						
+						Date d = startDate;
+						System.out.println("________________________________________________________");
+						System.out.println("start: "+d);
+						int headway = Integer.parseInt(freq.getProperty("headway_secs").toString());
+						while (d.before(currentDate))
+						{
+							d.setTime(d.getTime() + ONE_SECOND_IN_MILLIS * headway);
+						}
+						System.out.println("inainte: "+d);
+						System.out.println("curent: "+currentDate);
+						System.out.println ((currentDate.getTime()-d.getTime())/1000);
+						System.out.println("________________________________________________________");
+						return (d.getTime()-currentDate.getTime())/1000;
+					}
+			}
+		}
+		return 0;
 	}
 }
